f40cec9022731e38a39002f6690bb08039f6f967
scho123 <nvmsnach@gmail.com>
2024-08-26 18:52:06 +0900

    fix: import path 수정

--- src/main/java/com/ohgiraffers/mvcprojectgroup3/common/jdbc/JDBCTemplate.java
+++ src/main/java/com/ohgiraffers/mvcprojectgroup3/common/jdbc/JDBCTemplate.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.mvcprojectgroup3.common.jdbc;
 
-import com.ohgiraffers.mvc.common.config.ConfigLocation;
+import com.ohgiraffers.mvcprojectgroup3.common.config.ConfigLocation;
 
 import java.io.FileReader;
 import java.io.IOException;
