f3c9d4b88efee05365b50e16c9262b6470dcf200
dlskfk <naralee95@gmail.com>
2024-08-27 10:30:47 +0900

    insert 작성 중

--- src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/controller/InsertEmpServlet.java
+++ src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/controller/InsertEmpServlet.java
@@ -13,6 +13,53 @@ import java.io.IOException;
 @WebServlet("/employee/insert")
 public class InsertEmpServlet extends HttpServlet {
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String empName = req.getParameter("empName");
+        String empNo = req.getParameter("empNo");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        String deptCode = req.getParameter("deptCode");
+        String jobCode = req.getParameter("jobCode");
+        String salLevel = req.getParameter("salLevel");
+        int salary = Integer.parseInt(req.getParameter("salary"));
+        double bonus = Double.parseDouble(req.getParameter("bonus"));
+        String manageId = req.getParameter("managerId");
+        java.sql.Date hireDate = java.sql.Date.valueOf(req.getParameter("hireDate"));
+
+        EmployeeService empService = new EmployeeService();
+//        String newEmpId = empService.selectNewEmpId();        // selectNewEmpId 생성 후 주석 풀기
+
+        EmployeeDTO emp  = new EmployeeDTO();
+//        if(newEmpId !=null) emp.setEmpId(newEmpId);           // selectNewEmpId 생성 후 주석 풀기
+        emp.setEmpName(empName);
+        emp.setEmpNo(empNo);
+        emp.setEmail(email);
+        emp.setPhone(phone);
+        emp.setDeptCode(deptCode);
+        emp.setJobCode(jobCode);
+        emp.setSalLevel(salLevel);
+        emp.setSalary(salary);
+        emp.setBonus(bonus);
+        emp.setManagerId(manageId);
+        emp.setHireDate(hireDate);
+
+        System.out.println("insert request emp : " + emp);
+
+        int result = empService.insertEmp(emp);
+
+        String path = "";
+        if(result > 0) {
+            path = ""
+        }
 
 
+
+
+
+
+
+    }
 }
--- src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/service/EmployeeService.java
+++ src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/service/EmployeeService.java
@@ -10,7 +10,23 @@ import static com.ohgiraffers.mvcprojectgroup3.common.jdbc.JDBCTemplate.*;
 
 public class EmployeeService {
 
+/* 사원 등록 */
+    public int insertEmp(EmployeeDTO emp) {
 
+        Connection con = getConnection();
+
+        int result = empDAO.insertEmp(con, emp);
+
+        if (result > 0) {
+            commit(con);
+        } else {
+            rollback(con);
+        }
+
+        close(con);
+
+        return result;
+    }
 
 }
 
