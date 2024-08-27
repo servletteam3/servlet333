a413333b4e3f7d0748b6e0b117e573c43c3a38c8
manalee2015 <yanhongli86@gmail.com>
2024-08-27 12:31:24 +0900

    Mana's first commit

--- src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/dao/EmployeeDAO.java
+++ src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/dao/EmployeeDAO.java
@@ -1,15 +1,65 @@
 package com.ohgiraffers.mvcprojectgroup3.employee.model.dao;
 
+import com.ohgiraffers.mvcprojectgroup3.common.config.ConfigLocation;
+import com.ohgiraffers.mvcprojectgroup3.employee.model.dto.EmployeeDTO;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.Properties;
 
+import static com.ohgiraffers.mvcprojectgroup3.common.jdbc.JDBCTemplate.close;
 
 public class EmployeeDAO {
 
+    private final Properties prop;
 
+    public EmployeeDAO() {
 
+        prop = new Properties();
+        try {
+            prop.loadFromXML(new FileInputStream(ConfigLocation.MAPPER_LOCATION + "employee-mapper.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public EmployeeDTO selectEmpById(Connection con, String empId) {
+
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+
+        EmployeeDTO selectedEmp = null;
+
+        String query = prop.getProperty("selectEmpById");
+
+        try {
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, empId);
+
+            rset = pstmt.executeQuery();
+
+            if (rset.next()) {
+                selectedEmp = new EmployeeDTO();
+
+                selectedEmp.setEmpId(rset.getString("EMP_ID"));
+                selectedEmp.setEmpName(rset.getString("EMP_NAME"));
+                selectedEmp.setDeptCode(rset.getString("DEPT_CODE"));
+                selectedEmp.setJobCode(rset.getString("JOB_CODE"));
+                selectedEmp.setSalary(rset.getInt("SALARY"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rset);
+            close(pstmt);
+        }
+
+        return selectedEmp;
+    }
 }
--- src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/service/EmployeeService.java
+++ src/main/java/com/ohgiraffers/mvcprojectgroup3/employee/model/service/EmployeeService.java
@@ -4,14 +4,32 @@ import com.ohgiraffers.mvcprojectgroup3.employee.model.dao.EmployeeDAO;
 import com.ohgiraffers.mvcprojectgroup3.employee.model.dto.EmployeeDTO;
 
 import java.sql.Connection;
-import java.util.List;
+import java.sql.SQLException;
 
 import static com.ohgiraffers.mvcprojectgroup3.common.jdbc.JDBCTemplate.*;
 
 public class EmployeeService {
 
+    private final EmployeeDAO empDAO;
 
+    public EmployeeService() {
+        empDAO = new EmployeeDAO();
+    }
 
+    public EmployeeDTO selectOneEmpById(String empId) {
+
+        /* Connection 생성 */
+        Connection con = getConnection();
+
+        /* Connection과 함께 정보를 전달하여 조회한다. */
+        EmployeeDTO selectedEmp = empDAO.selectEmpById(con, empId);
+
+        /* connection 닫기 */
+        close(con);
+
+        /* 조회 결과를 반환한다. */
+        return selectedEmp;
+    }
 }
 
 
